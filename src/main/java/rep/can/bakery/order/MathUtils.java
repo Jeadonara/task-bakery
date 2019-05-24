83fdadbf2358c9cb709bf33d1e9fcd371b552e10
Can <m.can.tezel@gmail.com>
2019-05-24 12:47:39 +0300

    MathUtils Tests Implemented

---
+++ src/main/java/rep/can/bakery/order/MathUtils.java
@@ -0,0 +1,24 @@
+package rep.can.bakery.order;
+
+import java.util.List;
+import java.util.Map;
+
+public final class MathUtils {
+
+    private MathUtils() {
+    }
+
+    /**
+     * @param dividend number as dividend
+     * @param divisors numbers as divisors
+     * @return Divisor and Factor map
+     * If dividend can be divided by divisors until remainder equals to Zero
+     * Otherwise Empty Map
+     */
+    public static Map<Integer, Integer> getDivisionFactors(Integer dividend, List<Integer> divisors) {
+
+        return null;
+    }
+
+
+}
---
+++ src/test/java/rep/can/bakery/order/MathUtilsTest.java
@@ -0,0 +1,48 @@
+package rep.can.bakery.order;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+
+    @Test
+    public void givenEmptyDivisors_whenGetDivisionFactors_thenEmptyResultReturned() {
+
+        Map divisionFactors = MathUtils.getDivisionFactors(28, Collections.EMPTY_LIST);
+        assertTrue(divisionFactors.isEmpty());
+    }
+
+    @Test
+    public void givenAccurateDivisors_whenGetDivisionFactors_thenExpectedResultReturned() {
+        Map<Integer, Integer> divisionFactors = MathUtils.getDivisionFactors(14, Arrays.asList(2, 8));
+
+        assertEquals(0, divisionFactors.get(2).compareTo(3));
+        assertEquals(0, divisionFactors.get(8).compareTo(1));
+        assertEquals(2, divisionFactors.size());
+
+        Map<Integer, Integer> divisionFactors2 = MathUtils.getDivisionFactors(13, Arrays.asList(9, 5, 3));
+
+        assertEquals(0, divisionFactors2.get(5).compareTo(2));
+        assertEquals(0, divisionFactors2.get(3).compareTo(1));
+        assertEquals(2, divisionFactors2.size());
+
+
+    }
+
+    @Test
+    public void givenNotAccurateDivisors_whenGetDivisionFactors_thenEmptyResultReturned() {
+
+        Map divisionFactors = MathUtils.getDivisionFactors(28, Arrays.asList(30, 32));
+        assertTrue(divisionFactors.isEmpty());
+
+        Map divisionFactors2 = MathUtils.getDivisionFactors(14, Arrays.asList(4, 8));
+        assertTrue(divisionFactors2.isEmpty());
+
+
+    }
+}
