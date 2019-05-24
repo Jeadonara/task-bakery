ef9d97ad60b444881cd35526cae9d8e94e421f4c
Can <m.can.tezel@gmail.com>
2019-05-24 14:20:03 +0300

    Implementation of MathUtils

--- src/main/java/rep/can/bakery/order/MathUtils.java
+++ src/main/java/rep/can/bakery/order/MathUtils.java
@@ -1,5 +1,8 @@
 package rep.can.bakery.order;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,8 +20,38 @@ public final class MathUtils {
      */
     public static Map<Integer, Integer> getDivisionFactors(Integer dividend, List<Integer> divisors) {
 
-        return null;
+        divisors.sort(Collections.reverseOrder());
+
+        LinkedList<Integer> effectiveDivisors = new LinkedList<>(divisors);
+
+        Map<Integer, Integer> result;
+        Integer effectiveDividend;
+
+        do {
+            //Clear all previous calculation result to iterate on new divisor list
+            result = new HashMap<>();
+
+            //Set effective dividend as original dividend value to iterate on new divisor list
+            effectiveDividend = dividend;
+
+            for (int i = 0; i < effectiveDivisors.size() && effectiveDividend > 0; i++) {
+
+                Integer divisor = effectiveDivisors.get(i);
+
+                int remainder = effectiveDividend / divisor;
+
+                result.put(divisor, remainder);
+
+                effectiveDividend = effectiveDividend - (remainder * divisor);
+            }
+
+            //Remove the greatest divisor and try calculation again on remaining ones
+            effectiveDivisors.pop();
+
+        } while (effectiveDividend != 0 && effectiveDivisors.size() > 0);
+
+
+        return effectiveDividend == 0 ? result : Collections.EMPTY_MAP;
+
     }
-
-
 }
