52d95b1dc364c760ef25c14d8a115f251aef1726
Can <m.can.tezel@gmail.com>
2019-05-24 11:40:03 +0300

    Product Constant Definition for Default Products

---
+++ src/main/java/rep/can/bakery/product/Constants.java
@@ -0,0 +1,49 @@
+package rep.can.bakery.product;
+
+import rep.can.bakery.product.dto.Product;
+import rep.can.bakery.product.dto.ProductSaleDetail;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final List<Product> DEFAULT_PRODUCTS;
+
+    static {
+        Product vs5 = new Product("Vegemite Scroll", "VS5");
+        vs5.setSaleDetails(
+                Arrays
+                        .asList(
+                                ProductSaleDetail.of(3, BigDecimal.valueOf(6.99)),
+                                ProductSaleDetail.of(5, BigDecimal.valueOf(8.99))
+                        )
+        );
+
+        Product mb11 = new Product("Blueberry Muffin", "MB11");
+        vs5.setSaleDetails(
+                Arrays
+                        .asList(
+                                ProductSaleDetail.of(2, BigDecimal.valueOf(9.95)),
+                                ProductSaleDetail.of(5, BigDecimal.valueOf(16.95)),
+                                ProductSaleDetail.of(8, BigDecimal.valueOf(24.95))
+                        )
+        );
+
+        Product cf = new Product("Croissant", "CF");
+        vs5.setSaleDetails(
+                Arrays
+                        .asList(
+                                ProductSaleDetail.of(3, BigDecimal.valueOf(5.95)),
+                                ProductSaleDetail.of(5, BigDecimal.valueOf(9.95)),
+                                ProductSaleDetail.of(9, BigDecimal.valueOf(16.99))
+                        )
+        );
+
+        DEFAULT_PRODUCTS = Arrays.asList(vs5, mb11, cf);
+    }
+}
--- src/main/java/rep/can/bakery/product/service/DefaultProductService.java
+++ src/main/java/rep/can/bakery/product/service/DefaultProductService.java
@@ -7,6 +7,8 @@ import rep.can.bakery.product.dto.Product;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static rep.can.bakery.product.Constants.DEFAULT_PRODUCTS;
+
 class DefaultProductService implements ProductService {
 
     @Override
@@ -14,7 +16,7 @@ class DefaultProductService implements ProductService {
         List<String> productCodesToFilter = searchInput.getProductCodes();
 
 
-        List<Product> products = ProductService.getDefaultProducts();
+        List<Product> products = getDefaultProducts();
 
         List<Product> filteredProducts = products.stream()
                 .filter(product -> productCodesToFilter.contains(product.getCode()))
@@ -24,5 +26,9 @@ class DefaultProductService implements ProductService {
         output.setProducts(filteredProducts);
 
         return output;
+    }
+
+    private List<Product> getDefaultProducts() {
+        return DEFAULT_PRODUCTS;
     }
 }
