a10e047e4e295d9540b152914a117ec4d80b8c81
Can <m.can.tezel@gmail.com>
2019-05-24 11:45:17 +0300

    Product Service Test Implementation

--- src/test/java/rep/can/bakery/product/ProductServiceTest.java
+++ src/test/java/rep/can/bakery/product/ProductServiceTest.java
@@ -3,11 +3,16 @@ package rep.can.bakery.product;
 import org.junit.jupiter.api.Test;
 import rep.can.bakery.product.dto.GetProductsOutput;
 import rep.can.bakery.product.dto.GetProductsSearchInput;
+import rep.can.bakery.product.dto.Product;
 import rep.can.bakery.product.service.ProductService;
 import rep.can.bakery.product.service.ProductServiceFactory;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static java.util.Collections.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static rep.can.bakery.product.Constants.DEFAULT_PRODUCTS;
 
 public class ProductServiceTest {
 
@@ -25,6 +30,26 @@ public class ProductServiceTest {
 
     @Test
     public void givenDefaultProductCodes_whenGetProducts_thenDefaultProductsReturned() {
+
+        List<String> productCodesToSearch = DEFAULT_PRODUCTS
+                .stream()
+                .map(Product::getCode)
+                .collect(Collectors.toList());
+
+        GetProductsSearchInput searchInput = new GetProductsSearchInput();
+        searchInput.setProductCodes(productCodesToSearch);
+
+        GetProductsOutput searchOutput = productService.getProducts(searchInput);
+
+        List<Product> products = searchOutput.getProducts();
+
+        Boolean allDefaultProductsReturned = products
+                .stream()
+                .map(Product::getCode)
+                .collect(Collectors.toList())
+                .containsAll(productCodesToSearch);
+
+        assertTrue(allDefaultProductsReturned && products.size() == productCodesToSearch.size());
 
     }
 
