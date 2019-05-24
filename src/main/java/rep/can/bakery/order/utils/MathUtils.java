a607d2cd4b5f4ba77d4d12a86d1b202795da4a8f
Can <m.can.tezel@gmail.com>
2019-05-24 18:06:24 +0300

    Bug Solution Phase

--- src/main/java/rep/can/bakery/order/utils/MathUtils.java
+++ src/main/java/rep/can/bakery/order/utils/MathUtils.java
@@ -23,35 +23,63 @@ public final class MathUtils {
         divisors.sort(Collections.reverseOrder());
 
         LinkedList<Integer> effectiveDivisors = new LinkedList<>(divisors);
+        LinkedList<Integer> effectiveDivisorsClone = new LinkedList<>(effectiveDivisors);
 
         Map<Integer, Integer> result;
         Integer effectiveDividend;
+        LinkedList<Integer> numbersToIterate = new LinkedList<>(effectiveDivisors);
+        Integer holdNumber = effectiveDivisors.pop();
 
         do {
-            //Clear all previous calculation result to iterate on new divisor list
-            result = new HashMap<>();
-
-            //Set effective dividend as original dividend value to iterate on new divisor list
-            effectiveDividend = dividend;
-
-            for (int i = 0; i < effectiveDivisors.size() && effectiveDividend > 0; i++) {
-
-                Integer divisor = effectiveDivisors.get(i);
-
-                int remainder = effectiveDividend / divisor;
-
-                result.put(divisor, remainder);
-
-                effectiveDividend = effectiveDividend - (remainder * divisor);
+            if (numbersToIterate.size() == 1) {
+                holdNumber = effectiveDivisors.pop();
             }
 
-            //Remove the greatest divisor and try calculation again on remaining ones
-            effectiveDivisors.pop();
+            //Clear all previous calculation result to iterate on new divisor list
+            result = new HashMap<>();
+            effectiveDividend = getSequence(result, dividend, numbersToIterate);
+
+            if (effectiveDividend != 0) {
+                effectiveDivisors.pop();
+                numbersToIterate = merge(holdNumber, effectiveDivisors);
+            }
 
         } while (effectiveDividend != 0 && effectiveDivisors.size() > 0);
 
 
         return effectiveDividend == 0 ? result : Collections.EMPTY_MAP;
+
+    }
+
+    private static LinkedList<Integer> merge(Integer holdNumber, LinkedList<Integer> effectiveDivisors) {
+        LinkedList<Integer> output = new LinkedList<>();
+        output.add(holdNumber);
+        output.addAll(effectiveDivisors);
+
+        return output;
+    }
+
+    private static Integer getSequence(Map<Integer, Integer> resultToMutate, Integer dividend, LinkedList<Integer> effectiveDivisors) {
+        Integer effectiveDividend = dividend;
+
+        //Clear all previous calculation result to iterate on new divisor list
+        Map<Integer, Integer> result = new HashMap<>();
+
+        //Set effective dividend as original dividend value to iterate on new divisor list
+
+        for (int i = 0; i < effectiveDivisors.size() && effectiveDividend > 0; i++) {
+
+            Integer divisor = effectiveDivisors.get(i);
+
+            int remainder = effectiveDividend / divisor;
+
+            result.put(divisor, remainder);
+
+            effectiveDividend = effectiveDividend - (remainder * divisor);
+        }
+
+        resultToMutate.putAll(result);
+        return effectiveDividend;
 
     }
 }
