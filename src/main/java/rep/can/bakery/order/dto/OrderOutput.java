ca14b38cd3c8f3009016f262a206e5fe91375949
Can <m.can.tezel@gmail.com>
2019-05-24 14:47:56 +0300

    Order Service Implementation

--- src/main/java/rep/can/bakery/order/dto/OrderOutput.java
+++ src/main/java/rep/can/bakery/order/dto/OrderOutput.java
@@ -1,18 +1,21 @@
 package rep.can.bakery.order.dto;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 
 public class OrderOutput {
     private final Order order;
     private final BigDecimal totalPrice;
     private final List<OrderSaleDetail> saleDetails;
+    private final Boolean isEmpty;
 
 
-    public OrderOutput(Order order, BigDecimal totalPrice, List<OrderSaleDetail> saleDetails) {
+    private OrderOutput(Order order, BigDecimal totalPrice, List<OrderSaleDetail> saleDetails, Boolean isEmpty) {
         this.order = order;
         this.totalPrice = totalPrice;
         this.saleDetails = saleDetails;
+        this.isEmpty = isEmpty;
     }
 
     public Order getOrder() {
@@ -25,5 +28,13 @@ public class OrderOutput {
 
     public List<OrderSaleDetail> getSaleDetails() {
         return saleDetails;
+    }
+
+    public static OrderOutput emptyOutputOfOrder(Order order) {
+        return new OrderOutput(order, BigDecimal.ZERO, Collections.emptyList(), true);
+    }
+
+    public static OrderOutput of(Order order, BigDecimal totalPrice, List<OrderSaleDetail> saleDetails) {
+        return new OrderOutput(order, totalPrice, saleDetails, false);
     }
 }
--- src/main/java/rep/can/bakery/order/service/DefaultOrderService.java
+++ src/main/java/rep/can/bakery/order/service/DefaultOrderService.java
@@ -1,17 +1,92 @@
 package rep.can.bakery.order.service;
 
+import rep.can.bakery.order.MathUtils;
 import rep.can.bakery.order.dto.CreateOrdersInput;
 import rep.can.bakery.order.dto.CreateOrdersOutput;
+import rep.can.bakery.order.dto.Order;
+import rep.can.bakery.order.dto.OrderOutput;
+import rep.can.bakery.order.dto.OrderSaleDetail;
+import rep.can.bakery.product.dto.GetProductsOutput;
+import rep.can.bakery.product.dto.GetProductsSearchInput;
+import rep.can.bakery.product.dto.Product;
+import rep.can.bakery.product.dto.ProductSaleDetail;
 import rep.can.bakery.product.service.ProductService;
 import rep.can.bakery.product.service.ProductServiceFactory;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 class DefaultOrderService implements OrderService {
 
     private final ProductService productService = ProductServiceFactory.get();
 
     @Override
-    public CreateOrdersOutput createOrder(CreateOrdersInput orderInput) {
-        return null;
+    public CreateOrdersOutput createOrder(CreateOrdersInput input) {
+        List<Order> orders = input.getOrders();
+
+        Map<String, Product> codeProductMap = getAvailableProductsForOrders(orders);
+
+        List<OrderOutput> orderOutputs = orders
+                .stream()
+                .map(order ->
+                        Optional.ofNullable(codeProductMap.get(order.getProductCode()))
+                                .map(product -> getProductOrderOutput(product, order))
+                                .orElse(OrderOutput.emptyOutputOfOrder(order))
+                )
+                .collect(Collectors.toList());
+
+        CreateOrdersOutput output = new CreateOrdersOutput();
+        output.setOrderOutputs(orderOutputs);
+
+        return output;
     }
+
+    private Map<String, Product> getAvailableProductsForOrders(List<Order> orders) {
+
+        List<String> productCodesToSearch = orders
+                .stream()
+                .map(Order::getProductCode)
+                .collect(Collectors.toList());
+
+        GetProductsSearchInput searchInput = new GetProductsSearchInput();
+        searchInput.setProductCodes(productCodesToSearch);
+
+        GetProductsOutput output = productService.getProducts(searchInput);
+
+        return output
+                .getProducts()
+                .stream()
+                .collect(Collectors.toMap(Product::getCode, p -> p));
+
+    }
+
+    private OrderOutput getProductOrderOutput(Product product, Order order) {
+
+        Map<Integer, BigDecimal> saleSizePriceMap = product
+                .getSaleDetails()
+                .stream()
+                .collect(Collectors.toMap(ProductSaleDetail::getSaleSize, ProductSaleDetail::getPrice));
+
+        Map<Integer, Integer> sizedPackageAmountMap = MathUtils.getDivisionFactors(order.getAmount(), new ArrayList<>(saleSizePriceMap.keySet()));
+
+        List<OrderSaleDetail> orderSaleDetails = sizedPackageAmountMap.entrySet()
+                .stream()
+                .map(entry -> new OrderSaleDetail(entry.getValue(), entry.getKey(), saleSizePriceMap.get(entry.getKey())))
+                .collect(Collectors.toList());
+
+        BigDecimal totalPrice = orderSaleDetails
+                .stream()
+                .map(d -> d.getSalePrice().multiply(new BigDecimal(d.getAmount())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+
+        return OrderOutput.of(order, totalPrice, orderSaleDetails);
+
+    }
+
 
 }
